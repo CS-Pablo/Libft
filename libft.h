db8000b8d399c577d428e510ff6ff56515f6b2f4
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-09 19:59:07 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 19:09:35 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/09 19:31:22 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/09 19:59:07 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 #include <stdlib.h>
 #include <strings.h>
 
-int ft_atoi(char *str);
+int    ft_atoi (char *str);
 void    ft_bzero(void *b, size_t n);
 
 
