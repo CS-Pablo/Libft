d123c1b000ffddc1fb2e0f70f7fae53b377e4d3f
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-09 19:27:26 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 19:09:35 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/09 19:26:05 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/09 19:27:25 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+int ft_atoi(char *str);
 
 
 
