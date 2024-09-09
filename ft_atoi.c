9d8a8021432bc714cdddd0e48b0967fa478024fd
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-09 19:28:41 +0200

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 19:23:01 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/09 19:26:50 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/09 19:28:41 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,13 @@ int ft_atoi(char *str)
 	sign = 1;
 	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	while (str[i] == '-' || str[i] == '+')
+	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
 			sign = sign * -1;
 		i++;
 	}
+    
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		res = res * 10 + (str[i] - '0');
