dd6b1fb2015922a88fcd561b2a7976955cba5fb0
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-09 23:01:14 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 19:09:35 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/09 23:00:23 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/09 23:01:14 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ int	    ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n):
 void	*ft_memmove(void *dest, const void *src, size_t n):
 void	*ft_memset(void *s, int c, size_t n);
+char	*ft_strchr(const char *s, int c);
 
 #endif
