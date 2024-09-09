242116591874448fba634573db0660fa886dba1e
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-09 20:39:22 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 19:09:35 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/09 20:12:55 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/09 20:39:22 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@
 int    ft_atoi (char *str);
 void    ft_bzero(void *b, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
+int		ft_isalnum(int c);
 
 #endif
