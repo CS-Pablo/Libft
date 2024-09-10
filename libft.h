848ce7f05fb9d82ef41f6834933c2ba2bfb17622
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-10 16:59:18 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 19:09:35 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/10 16:46:23 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/10 16:59:18 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ int    ft_isdigit(int c);
 int	    ft_isprint(int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	    ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n):
-void	*ft_memmove(void *dest, const void *src, size_t n):
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
