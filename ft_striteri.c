1d3b839a69942550a07e4f64e2a7bed6d78112f5
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-10 17:28:25 +0200

    Update ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,23 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 17:14:27 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/10 17:15:37 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/10 17:28:25 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int i;
+
+	i = 0;
+	if (s)
+	{
+		while (s[i])
+		{
+			(*f)(i, s + i);
+			i++;
+		}
+	}
+}
