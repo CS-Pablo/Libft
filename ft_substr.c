60882c0b4f256444861c7e5ba38b041475957fb5
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2024-09-10 17:23:10 +0200

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,34 @@
 /*   By: csauron <csauron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 17:12:44 by csauron           #+#    #+#             */
-/*   Updated: 2024/09/10 17:13:06 by csauron          ###   ########.fr       */
+/*   Updated: 2024/09/10 17:23:10 by csauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*tab;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!s)
+		return (NULL);
+	tab = (char *)malloc(sizeof(*s) * (len + 1));
+	if (!tab)
+		return (NULL);
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			tab[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	tab[j] = '\0';
+	return (tab);
+}
